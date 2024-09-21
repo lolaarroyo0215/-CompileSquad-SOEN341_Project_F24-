491d3357ab338e433c701cacde3ed50bb339f665
katzmaria <maria_christine.catiche@hotmail.ca>
2024-09-21 15:41:38 -0400

    Update create_database.sql

--- database/create_database.sql
+++ database/create_database.sql
@@ -12,6 +12,19 @@ CREATE TABLE IF NOT EXISTS students (
     team_id INT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+-- 2.1 Add students (after table is created)
+INSERT INTO students (first_name, last_name, email, password_hash)
+VALUES 
+('Elie', 'John', 'elie.john@example.ca', SHA2('password123', 256)),
+('Alex', 'Smith', 'alex.smith@example.com', SHA2('password123', 256)),
+('Emily', 'Johnson', 'emily.johnson@example.com', SHA2('password123', 256)),
+('Michael', 'Williams', 'michael.williams@example.com', SHA2('password123', 256)),
+('Sophia', 'Jones', 'sophia.jones@example.com', SHA2('password123', 256)),
+('James', 'Brown', 'james.brown@example.com', SHA2('password123', 256)),
+('Olivia', 'Davis', 'olivia.davis@example.com', SHA2('password123', 256)),
+('Liam', 'Miller', 'liam.miller@example.com', SHA2('password123', 256)),
+('Ava', 'Wilson', 'ava.wilson@example.com', SHA2('password123', 256)),
+('Noah', 'Moore', 'noah.moore@example.com', SHA2('password123', 256));
 
 -- 3. Create the 'instructors' Table
 CREATE TABLE IF NOT EXISTS instructors (
@@ -22,3 +35,13 @@ CREATE TABLE IF NOT EXISTS instructors (
     password_hash VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+-- 3.1 Add an instructor (after table is created)
+INSERT INTO instructors (first_name, last_name, email, password_hash)
+VALUES 
+('Joumana', 'Dargham', 'joumana.dargham@concordia.ca', SHA2('password123', 256)),
+('Michael', 'Taylor', 'michael.taylor@example.com', SHA2('password123', 256)),
+('Sophia', 'Nguyen', 'sophia.nguyen@example.com', SHA2('password123', 256));
+
+-- 4 The table to put the instructor
+SELECT * FROM instructors -- Select operator allows to see the selected instructor to be into the data base
+
