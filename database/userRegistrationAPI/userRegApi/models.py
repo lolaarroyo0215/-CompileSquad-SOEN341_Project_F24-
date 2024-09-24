6aa3c32f4d0cd84e34e400d19df9b2ac50d3db77
Sofia Sanchez <sofiasanchez@sofias-air-7.wireless.concordia.ca>
2024-09-24 09:14:15 -0400

    user registration API compleated, along with passwordhashing

--- database/userRegistrationAPI/userRegApi/models.py
+++ database/userRegistrationAPI/userRegApi/models.py
@@ -1,23 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-class Student(models.Model):
+class Student(AbstractBaseUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
-    password_hash = models.CharField(max_length=255)
+    student_id = models.IntegerField(null=True, blank=True)
     team_id = models.IntegerField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    password = models.CharField(max_length=128)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
     
-class Instructor(models.Model):
-    instructor_id = models.IntegerField()
+class Instructor(AbstractBaseUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
-    password_hash = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
+    instructor_id = models.IntegerField()
+    password = models.CharField(max_length=128)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
