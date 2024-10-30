fbda14a34470a763328f8c133590cc1e89eefc5b
Sofia Sanchez <sofia24sanchez@yahoo.ca>
2024-10-30 16:12:33 -0400

    fixed teacher logout button

--- FrontEnd/src/mainTeacherPage.jsx
+++ FrontEnd/src/mainTeacherPage.jsx
@@ -4,6 +4,15 @@ import { useNavigate } from 'react-router-dom';
 import './index.css';
 
 export default function MainTeacherPage() {
+
+  const navigate = useNavigate();
+
+  function handleLogout(event) {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
+    navigate('/');
+  }
+
   return (
     <div className="bg-slate-200 min-h-screen flex flex-col">
       <nav className="bg-red-900 p-4 flex justify-between items-center">
@@ -13,7 +22,7 @@ export default function MainTeacherPage() {
         </div>
         <div className="flex space-x-10">
           <span className="text-white hover:text-red-950 cursor-pointer">Profile</span>
-          <span className="text-white hover:text-red-950 cursor-pointer">Log Out</span>
+          <button type='button' onClick={handleLogout} className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-red-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Log Out</button>
         </div>
       </nav>
 
